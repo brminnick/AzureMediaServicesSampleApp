639b1558e13bb195a397b6146ce0358ffd747246
brminnick <brandon.r.minnick@gmail.com>
2017-08-23 16:53:40 -0700

    First Commit

---
+++ AzureMediaServicesSampleApp/App.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+
+namespace AzureMediaServicesSampleApp
+{
+    public class App : Application
+    {
+        public App() =>
+            MainPage = new ContentPage
+            {
+                Content = new WebView
+                {
+                    Source = "https://amssamples.streaming.mediaservices.windows.net/2e91931e-0d29-482b-a42b-9aadc93eb825/AzurePromo.mp4"
+                }
+            };
+    }
+}
