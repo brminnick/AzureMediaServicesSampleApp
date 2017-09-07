6c0339fc9767eb2fd6bb358851e212e25fd8cb5b
brminnick <brandon.r.minnick@gmail.com>
2017-09-07 13:58:31 -0700

    Updated VideoUrl to use the streaming manifest

--- AzureMediaServicesSampleApp/Constants/MediaConstants.cs
+++ AzureMediaServicesSampleApp/Constants/MediaConstants.cs
@@ -3,6 +3,6 @@ namespace AzureMediaServicesSampleApp
 {
     public static class MediaConstants
     {
-        public const string VideoUrl = @"https://amssamples.streaming.mediaservices.windows.net/2e91931e-0d29-482b-a42b-9aadc93eb825/AzurePromo.mp4";
+        public const string VideoUrl = @"https://amssamples.streaming.mediaservices.windows.net/91492735-c523-432b-ba01-faba6c2206a2/AzureMediaServicesPromo.ism/manifest(format=m3u8-aapl)";
     }
 }
