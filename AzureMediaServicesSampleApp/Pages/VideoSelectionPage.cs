32de5c82a98ddf41dd489a0e8c745a0db3d54fdd
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-12-08 12:55:06 -0800

    Update NuGet Packages

--- AzureMediaServicesSampleApp/Pages/VideoSelectionPage.cs
+++ AzureMediaServicesSampleApp/Pages/VideoSelectionPage.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using Xamarin.CommunityToolkit.Markup;
 using Xamarin.Forms;
 
 namespace AzureMediaServicesSampleApp
@@ -8,38 +8,26 @@ namespace AzureMediaServicesSampleApp
     {
         public VideoSelectionPage()
         {
-            var webViewPageButton = new Button { Text = "Watch As WebView" };
-            webViewPageButton.Clicked += HandleWebViewPageButtonClicked;
-
-            var nativeVideoPlayerButton = new Button { Text = "Watch in Native Video Player" };
-            nativeVideoPlayerButton.Clicked += HandleNativeVideoPlayerButtonClicked;
-
             Title = "Choose a Video";
 
-            var stackLayout = new StackLayout
-            {
-                HorizontalOptions = LayoutOptions.Center,
-                VerticalOptions = LayoutOptions.Center,
 
-                Children = {
-                    webViewPageButton,
-                    nativeVideoPlayerButton
+            Content = new StackLayout
+            {
+                Children =
+                {
+                    new Button { Text = "Watch As WebView" }
+                        .Invoke(webViewPageButton => webViewPageButton.Clicked += HandleWebViewPageButtonClicked),
+
+                    new Button { Text = "Watch in Native Video Player" }
+                        .Invoke(nativeVideoPlayerButton => nativeVideoPlayerButton.Clicked += HandleNativeVideoPlayerButtonClicked)
                 }
-            };
+            }.Center().Assign(out StackLayout stackLayout);
 
             if (Device.RuntimePlatform is Device.Android)
             {
-                var androidEmulatorLabel = new Label
-                {
-                    Text = "Note: Video playback may fail when on an Android emulator\n\nPlease run this sample on a physical Android Device",
-                    FontAttributes = FontAttributes.Italic,
-                    HorizontalTextAlignment = TextAlignment.Center
-                };
-
-                stackLayout.Children.Add(androidEmulatorLabel);
+                stackLayout.Children.Add(new Label { Text = "Note: Video playback may fail when on an Android emulator\n\nPlease run this sample on a physical Android Device" }
+                                            .Font(italic: true).TextCenter());
             }
-
-            Content = stackLayout;
         }
 
         async void HandleWebViewPageButtonClicked(object sender, EventArgs e) => await Navigation.PushAsync(new VideoWebViewPage());
